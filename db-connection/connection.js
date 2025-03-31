aa7f1394c8907901467f10389500a912d924d8e9
Tushar Katore <144722798+tusharRkatore@users.noreply.github.com>
2025-03-31 19:40:04 +0530

    Update connection.js

--- db-connection/connection.js
+++ db-connection/connection.js
@@ -1,19 +1,15 @@
 require('dotenv').config();
-const mongoose = require("mongoose");
-
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/myDatabase";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true, // Optional (not required in latest Mongoose)
-      useUnifiedTopology: true, // Optional (not required in latest Mongoose)
-    });
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error);
-    process.exit(1); // Stop the process if DB connection fails
-  }
-};
-
-module.exports = connectDB;
+const mongoose=require('mongoose');
+const URI=process.env.MONGODB_URI;
+const connectDB=async()=>{
+    try{
+      await  mongoose.connect(URI);
+      console.log("Connection Successful");
+    }
+    catch(error)
+    {
+        console.error("connection Failed");
+        process.exit(0);
+    }
+}
+module.exports=connectDB;
